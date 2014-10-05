2b8fb8b7be9af65d0eb30c6a8cfb3e5a8ca6f017
Lars Kellogg-Stedman <lars@redhat.com>
2014-10-05 15:10:51 -0400

    make keystone use new service discovery mechanism
    Change-Id: I25d7aecc55f0db4e80c1feac0130d1dca7f0d39a

--- docker/keystone/start.sh
+++ docker/keystone/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Exit the container if MariaDB is not yet up - then depend on kube to restart
-if [ -z "$MARIADB_PORT_3306_TCP_PORT" ]; then
-    exit 1
-fi
+MY_IP=$(ip route get $(ip route | awk '$1 == "default" {print $3}') |
+    awk '$4 == "src" {print $5}')
 
+: ${PUBLIC_IP:=$MY_IP}
 : ${KEYSTONE_ADMIN_PASSWORD:=kolla}
 : ${ADMIN_TENANT_NAME:=admin}
 
@@ -16,7 +15,23 @@ if ! [ "$KEYSTONE_DB_PASSWORD" ]; then
     KEYSTONE_DB_PASSWORD=$(openssl rand -hex 15)
 fi
 
-mysql -h ${MARIADB_PORT_3306_TCP_ADDR} -u root -p${DB_ROOT_PASSWORD} mysql <<EOF
+if [ ! -f /startconfig ]; then
+    cat > /startconfig <<EOF
+PUBLIC_IP=$PUBLIC_IP
+KEYSTONE_ADMIN_PASSWORD=$KEYSTONE_ADMIN_PASSWORD
+ADMIN_TENANT_NAME=$ADMIN_TENANT_NAME
+KEYSTONE_ADMIN_TOKEN=$KEYSTONE_ADMIN_TOKEN
+KEYSTONE_DB_PASSWORD=$KEYSTONE_DB_PASSWORD
+EOF
+fi
+
+# wait for mysql to start
+while !  mysql -h mariadb -u root -p${DB_ROOT_PASSWORD} -e "select 1" mysql > /dev/null 2>&1; do
+    echo "waiting for mysql..."
+    sleep 1
+done
+
+mysql -h mariadb -u root -p${DB_ROOT_PASSWORD} mysql <<EOF
 CREATE DATABASE IF NOT EXISTS keystone;
 GRANT ALL PRIVILEGES ON keystone.* TO
     'keystone'@'%' IDENTIFIED BY '${KEYSTONE_DB_PASSWORD}'
@@ -25,7 +40,7 @@ EOF
 crudini --set /etc/keystone/keystone.conf \
     database \
     connection \
-    "mysql://keystone:${KEYSTONE_DB_PASSWORD}@${MARIADB_PORT_3306_TCP_ADDR}:${MARIADB_PORT_3306_TCP_PORT}/keystone"
+    "mysql://keystone:${KEYSTONE_DB_PASSWORD}@mariadb/keystone"
 crudini --set /etc/keystone/keystone.conf \
     DEFAULT \
     admin_token \
@@ -36,29 +51,21 @@ crudini --del /etc/keystone/keystone.conf \
 crudini --del /etc/keystone/keystone.conf \
     DEFAULT \
     log_dir
-crudini --set /etc/keystone/keystone.conf DEFAULT use_stderr True
+crudini --set /etc/keystone/keystone.conf \
+    DEFAULT \
+    use_stderr \
+    True
 
 cat /etc/keystone/keystone.conf
 
 /usr/bin/keystone-manage db_sync
 /usr/bin/keystone-manage pki_setup --keystone-user keystone --keystone-group keystone
 
-MY_IP=$(ip route get $(ip route | awk '$1 == "default" {print $3}') |
-    awk '$4 == "src" {print $5}')
-if [ -z "$KEYSTONE_ADMIN_PORT_35357_TCP_ADDR" ]; then
-    KEYSTONE_ADMIN_PORT_35357_TCP_ADDR=$MY_IP
-fi
-if [ -z "$KEYSTONE_PUBLIC_PORT_5000_TCP_ADDR" ]; then
-    KEYSTONE_PUBLIC_PORT_5000_TCP_ADDR=$MY_IP
-fi
-
 /usr/bin/keystone-all &
 PID=$!
 
 export SERVICE_TOKEN="${KEYSTONE_ADMIN_TOKEN}"
 export SERVICE_ENDPOINT="http://127.0.0.1:35357/v2.0"
-SERVICE_ENDPOINT_ADMIN="http://${KEYSTONE_ADMIN_PORT_35357_TCP_ADDR}:35357/v2.0"
-SERVICE_ENDPOINT_USER="http://${KEYSTONE_PUBLIC_PORT_5000_TCP_ADDR}:5000/v2.0"
 
 # wait for keystone to become active
 while ! curl -o /dev/null -s --fail ${SERVICE_ENDPOINT}; do
@@ -67,8 +74,9 @@ done
 
 crux user-create -n admin -p "${KEYSTONE_ADMIN_PASSWORD}" -t admin -r admin
 crux endpoint-create -n keystone -t identity \
-    -I "${SERVICE_ENDPOINT_USER}" \
-    -A "${SERVICE_ENDPOINT_ADMIN}"
+    -I "http://keystone:5000/v2.0" \
+    -A "http://keystone:35357/v2.0" \
+    -P "http://${PUBLIC_IP}:5000/v2.0"
 
 kill -TERM $PID
 
