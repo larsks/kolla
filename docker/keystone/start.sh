27b78393b07374c1dc9714fcb8b1eee1426ff939
Lars Kellogg-Stedman <lars@redhat.com>
2014-10-07 13:50:32 -0400

    added keystone replicationcontroller
    Change-Id: I2c6f3921e66589fdc3a51e4fb57b005c10a2ae89

--- docker/keystone/start.sh
+++ docker/keystone/start.sh
@@ -8,14 +8,9 @@ MY_IP=$(ip route get $(ip route | awk '$1 == "default" {print $3}') |
 : ${PUBLIC_IP:=$MY_IP}
 : ${KEYSTONE_ADMIN_PASSWORD:=kolla}
 : ${ADMIN_TENANT_NAME:=admin}
-
-if ! [ "$KEYSTONE_ADMIN_TOKEN" ]; then
-    KEYSTONE_ADMIN_TOKEN=$(openssl rand -hex 15)
-fi
-
-if ! [ "$KEYSTONE_DB_PASSWORD" ]; then
-    KEYSTONE_DB_PASSWORD=$(openssl rand -hex 15)
-fi
+: ${KEYSTONE_ADMIN_TOKEN:=ADMINTOKEN}
+: ${KEYSTONE_DB_PASSWORD:=secret}
+: ${KEYSTONE_ROLE:=minion}
 
 if [ ! -f /startconfig ]; then
     cat > /startconfig <<EOF
@@ -26,18 +21,6 @@ KEYSTONE_ADMIN_TOKEN=$KEYSTONE_ADMIN_TOKEN
 KEYSTONE_DB_PASSWORD=$KEYSTONE_DB_PASSWORD
 EOF
 fi
-
-# wait for mysql to start
-while !  mysql -h 127.0.0.1 -u root -p${DB_ROOT_PASSWORD} -e "select 1" mysql > /dev/null 2>&1; do
-	echo "waiting for mysql..."
-	sleep 1
-done
-
-mysql -h 127.0.0.1 -u root -p${DB_ROOT_PASSWORD} mysql <<EOF
-CREATE DATABASE IF NOT EXISTS keystone;
-GRANT ALL PRIVILEGES ON keystone.* TO
-    'keystone'@'%' IDENTIFIED BY '${KEYSTONE_DB_PASSWORD}'
-EOF
 
 crudini --set /etc/keystone/keystone.conf \
     DEFAULT \
@@ -70,29 +53,63 @@ crudini --set /etc/keystone/keystone.conf \
     config_file \
     /usr/share/keystone/keystone-dist-paste.ini
 
-cat /etc/keystone/keystone.conf
+grep -v '^$|^#' /etc/keystone/keystone.conf
 
-/usr/bin/keystone-manage db_sync
-/usr/bin/keystone-manage pki_setup --keystone-user keystone --keystone-group keystone
+if [ "$KEYSTONE_ROLE" = "master" ]; then
 
-/usr/bin/keystone-all &
-PID=$!
+    crudini --set /etc/keystone/keystone.conf \
+        DEFAULT \
+        admin_port \
+        35358
 
-export SERVICE_TOKEN="${KEYSTONE_ADMIN_TOKEN}"
-export SERVICE_ENDPOINT="http://127.0.0.1:35357/v2.0"
+    if [ -z "$DB_ROOT_PASSWORD" ]; then
+        echo "ERROR: missing DB_ROOT_PASSWORD" >&2
+        exit 1
+    fi
 
-# wait for keystone to become active
-while ! curl -o /dev/null -s --fail ${SERVICE_ENDPOINT}; do
-    sleep 1;
-done
+    # wait for mysql to start
+    while !  mysql -h 127.0.0.1 -u root -p${DB_ROOT_PASSWORD} -e "select 1" mysql > /dev/null 2>&1; do
+        echo "waiting for mysql..."
+        sleep 1
+    done
 
-crux user-create -n admin -p "${KEYSTONE_ADMIN_PASSWORD}" -t admin -r admin
-crux endpoint-create -n keystone -t identity \
-    -I "http://127.0.0.1:5000/v2.0" \
-    -P "http://${PUBLIC_IP}:5000/v2.0" \
-    -A "http://127.0.0.1:35357/v2.0"
+    mysql -h 127.0.0.1 -u root -p"${DB_ROOT_PASSWORD}" mysql <<EOF
+CREATE DATABASE IF NOT EXISTS keystone;
+GRANT ALL PRIVILEGES ON keystone.* TO
+    'keystone'@'%' IDENTIFIED BY '${KEYSTONE_DB_PASSWORD}'
+EOF
 
-kill -TERM $PID
+    /usr/bin/keystone-manage db_sync
+    /usr/bin/keystone-manage pki_setup --keystone-user keystone --keystone-group keystone
+
+    /usr/bin/keystone-all &
+    PID=$!
+
+    export SERVICE_TOKEN="${KEYSTONE_ADMIN_TOKEN}"
+    export SERVICE_ENDPOINT="http://${MY_IP}:35358/v2.0"
+
+    # wait for keystone to become active
+    while ! curl -o /dev/null -s --fail ${SERVICE_ENDPOINT}; do
+        sleep 1;
+    done
+
+    crux user-create -n admin -p "${KEYSTONE_ADMIN_PASSWORD}" -t admin -r admin
+    crux endpoint-create -n keystone -t identity \
+        -I "http://127.0.0.1:5000/v2.0" \
+        -P "http://${PUBLIC_IP}:5000/v2.0" \
+        -A "http://127.0.0.1:35357/v2.0"
+
+    kill -TERM $PID
+else
+    export SERVICE_TOKEN="${KEYSTONE_ADMIN_TOKEN}"
+    export SERVICE_ENDPOINT="http://127.0.0.1:35358/v2.0"
+
+    # wait for keystone to become active
+    while ! curl -o /dev/null -s --fail ${SERVICE_ENDPOINT}; do
+        echo "waiting for keystone..."
+        sleep 1;
+    done
+fi
 
 echo "Running keystone service."
 exec /usr/bin/keystone-all
